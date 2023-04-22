e0a25fde22bd74647d7da391f7935b54ece353fa
Avalas53 <nagylaszlo635@gmail.com>
2023-04-22 13:48:58 +0200

    creates Card view and checks if it exists; waitAndClick method created

--- petstoreTest.py
+++ petstoreTest.py
@@ -1,11 +1,18 @@
 import unittest
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class MyTestCase(unittest.TestCase):
+
+    def waitAndClick(self, oldalelem):
+        self.oldalelem = oldalelem
+        wait = WebDriverWait(self.meghajto, 10)
+        wait.until(EC.element_to_be_clickable((By.ID, oldalelem)))
+        self.meghajto.find_element(By.ID, oldalelem).click()
 
     def setUp(self):
         self.meghajto = webdriver.Chrome()
@@ -14,16 +21,10 @@ class MyTestCase(unittest.TestCase):
         meghajto = self.meghajto
         meghajto.maximize_window()
         meghajto.get("https://training.testifi.io/pets")
-        time.sleep(2)
-        petgomb = meghajto.find_element(By.ID, "menu")
-        petgomb.click()
-        time.sleep(1)
-        addview = meghajto.find_element(By.ID, "menu__add-view")
-        addview.click()
-        time.sleep(1)
-        cardview = meghajto.find_element(By.ID, "view-menu__card")
-        cardview.click()
-        time.sleep(1)
+        self.waitAndClick("menu")
+        self.waitAndClick("menu__add-view")
+        self.waitAndClick("view-menu__card")
+        self.assertIn("Card View", meghajto.page_source)
 
     def tearDown(self):
         self.meghajto.close()
